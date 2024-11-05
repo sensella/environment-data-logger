7f7102ceda9dbdc0b597713649bb027bc776ee9e
Senthil Kumar Sellapillai <senthil.sellapillai@gmail.com>
2024-11-05 17:50:16 +0530

    python

---
+++ main.py
@@ -0,0 +1,91 @@
+def on_log_full():
+    global Logging
+    Logging = False
+    basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+datalogger.on_log_full(on_log_full)
+
+def on_button_pressed_a():
+    global Logging, hour
+    Logging = True
+    if hour < 23:
+        hour += 1
+    else:
+        hour = 0
+    basic.show_icon(IconNames.YES)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_sound_loud():
+    basic.show_number(input.magnetic_force(Dimension.X))
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+
+def on_button_pressed_ab():
+    global Logging
+    Logging = False
+    basic.show_icon(IconNames.SKULL)
+    datalogger.delete_log()
+    datalogger.set_column_titles("Temperature", "Light")
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global Logging, minute
+    Logging = False
+    if minute < 59:
+        minute += 1
+    else:
+        minute = 0
+    basic.show_icon(IconNames.NO)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_shake():
+    pass
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_logo_pressed():
+    basic.show_number(input.temperature())
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+hour = 0
+minute = 0
+Logging = False
+Logging = False
+ampm = False
+time = ""
+adjust = 0
+minute = 0
+hour = 0
+datalogger.set_column_titles("Temperature", "Light")
+time = "" + time + ":"
+if minute < 10:
+    time = "" + time + "0"
+time = "" + time + str(minute)
+basic.show_string(time)
+
+def on_every_interval():
+    if Logging:
+        basic.show_icon(IconNames.HEART)
+        datalogger.log(datalogger.create_cv("Temperature", input.temperature()),
+            datalogger.create_cv("Light", input.light_level()))
+        basic.clear_screen()
+loops.every_interval(60000, on_every_interval)
+
+def on_forever():
+    global minute, hour
+    basic.pause(60000)
+    if minute < 59:
+        minute += 1
+    else:
+        minute = 0
+        if hour < 23:
+            hour += 1
+        else:
+            hour = 0
+    if input.magnetic_force(Dimension.X) < 100:
+        music.play(music.string_playable("- - - - - - - - ", 120),
+            music.PlaybackMode.UNTIL_DONE)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-datalogger.onLogFull(function () {
+datalogger.onLogFull(function on_log_full() {
+    
     Logging = false
     basic.showLeds(`
         # # # # #
@@ -8,40 +9,42 @@ datalogger.onLogFull(function () {
         # # # # #
         `)
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     Logging = true
     if (hour < 23) {
         hour += 1
     } else {
         hour = 0
     }
+    
     basic.showIcon(IconNames.Yes)
 })
-input.onSound(DetectedSound.Loud, function () {
+input.onSound(DetectedSound.Loud, function on_sound_loud() {
     basic.showNumber(input.magneticForce(Dimension.X))
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
     Logging = false
     basic.showIcon(IconNames.Skull)
     datalogger.deleteLog()
-    datalogger.setColumnTitles(
-    "Temperature",
-    "Light"
-    )
+    datalogger.setColumnTitles("Temperature", "Light")
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     Logging = false
     if (minute < 59) {
         minute += 1
     } else {
         minute = 0
     }
+    
     basic.showIcon(IconNames.No)
 })
-input.onGesture(Gesture.Shake, function () {
-	
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     basic.showNumber(input.temperature())
 })
 let hour = 0
@@ -53,27 +56,24 @@ let time = ""
 let adjust = 0
 minute = 0
 hour = 0
-datalogger.setColumnTitles(
-"Temperature",
-"Light"
-)
+datalogger.setColumnTitles("Temperature", "Light")
 time = "" + time + ":"
 if (minute < 10) {
     time = "" + time + "0"
 }
-time = "" + time + minute
+
+time = "" + time + ("" + minute)
 basic.showString(time)
-loops.everyInterval(60000, function () {
+loops.everyInterval(60000, function on_every_interval() {
     if (Logging) {
         basic.showIcon(IconNames.Heart)
-        datalogger.log(
-        datalogger.createCV("Temperature", input.temperature()),
-        datalogger.createCV("Light", input.lightLevel())
-        )
+        datalogger.log(datalogger.createCV("Temperature", input.temperature()), datalogger.createCV("Light", input.lightLevel()))
         basic.clearScreen()
     }
+    
 })
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     basic.pause(60000)
     if (minute < 59) {
         minute += 1
@@ -84,8 +84,11 @@ basic.forever(function () {
         } else {
             hour = 0
         }
+        
     }
+    
     if (input.magneticForce(Dimension.X) < 100) {
         music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
     }
+    
 })
